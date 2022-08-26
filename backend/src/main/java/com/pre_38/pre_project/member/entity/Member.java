dec226481d17e3225e063fe2da1f33d410bab4eb
ds02168 <1995musso@gmail.com>
2022-08-26 11:24:22 +0900

    레포지토리 구조 변경:스프링을 루트에서 backend로 이동

---
+++ backend/src/main/java/com/pre_38/pre_project/member/entity/Member.java
@@ -0,0 +1,26 @@
+package com.pre_38.pre_project.member.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column
+    private String avatar;
+
+    @Column(nullable = false)
+    private LocalDateTime date = LocalDateTime.now();
+}
